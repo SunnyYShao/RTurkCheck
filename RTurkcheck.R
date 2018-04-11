04edade6cba5014deef222baeca1017c7cad0b53
Loren Collingwood <loren.collingwood@ucr.edu>
2018-04-11 15:48:27 -0700

    Added aov_treat_compare function

--- RTurkcheck.R
+++ RTurkcheck.R
@@ -121,4 +121,26 @@ sum_func <- function(dat){
   return(data.frame(dat_min, dat_max, dat_mean, dat_sd))
 }
 
+########################################################
+# aov_treat_compare: Check Across Treatment Conditions #
+# ANOVA test across treatment conditions               #
+########################################################
+
+aov_treat_compare <- function (dat, var_names, treat_var){
   
+  # dat = dataframe, the final survey dataframe
+  # var_names = character vector of demographic variables to compare across treatment condition
+  # treat_var = character vector (1) name of treatment indicator 
+  
+  # Conduct ANOVA test and put into matrix for table output #
+  aov_mat <- matrix(NA, nrow=length(var_names), ncol=2)
+  for (i in 1:length(aov_vec)){
+    aov_mat[i,] <- unlist ( summary ( aov(formula ( 
+    paste (var_names[i], "~ as.factor(", treat_var, ")", sep="" )), data = dat )))[c(7,9)]
+  }
+  # Label Datasets #
+  colnames(aov_mat) <- c("F-Value", "P-Value")
+  rownames(aov_mat) <- var_names
+  aov_mat <- round(aov_mat, 4)
+  return (aov_mat)
+}
