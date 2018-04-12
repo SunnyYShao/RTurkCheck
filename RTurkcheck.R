7b9f7af8a3f89e665fc4e1c5dd22cad19e754096
Loren Collingwood <loren.collingwood@ucr.edu>
2018-04-12 16:09:05 -0700

    Updated sum_func, to include median, and table column labels.

--- RTurkcheck.R
+++ RTurkcheck.R
@@ -108,17 +108,20 @@ t_generalize <- function(w1, w2, cnames1, cnames2) {
 }
 
 
-##################################
-# Variable Desriptive Statistics #
-##################################
+###################################
+# Variable Descriptive Statistics #
+###################################
 
 sum_func <- function(dat){
-  
+  # dataset of variables you want summaries on
   dat_min <- apply(dat, 2, min, na.rm=T)
   dat_max <- apply(dat, 2, max, na.rm=T)
-  dat_mean <- apply(dat, 2, mean, na.rm=T)
-  dat_sd <- apply(dat, 2, sd, na.rm=T)
-  return(data.frame(dat_min, dat_max, dat_mean, dat_sd))
+  dat_mean <- round(apply(dat, 2, mean, na.rm=T), 4)
+  dat_med <- round(apply(dat, 2, median, na.rm=T), 4)
+  dat_sd <- round(apply(dat, 2, sd, na.rm=T), 4)
+  out <- data.frame(dat_min, dat_max, dat_mean, dat_med, dat_sd)
+  colnames(out) <- c("Minimum", "Maximum", "Mean","Median", "Std.Dev")
+  return(out)
 }
 
 ########################################################
