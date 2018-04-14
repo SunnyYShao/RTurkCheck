654de7c9897303a4166f924721e595c539b25d64
skdreier <skdreier@gmail.com>
2018-04-14 18:39:04 -0500

    more updates 2018-04-14 18:38

--- RTurkcheck.R
+++ RTurkcheck.R
@@ -151,15 +151,29 @@ aov_treat_compare <- function (dat, var_names, treat_var){
 
 ### Sarah's Snowy Saturday Night ###
 
+# read in datasets
 wave1 <- read.csv("w1.csv", header = TRUE)
 wave2 <- read.csv("w2.csv", header = TRUE)
 
+# create wave dummies (2 different ways)
+wave1$wave <- rep(1, dim(wave1)[1])
+wave2$wave <- 2
+
+# look at variable names
 colnames(wave1)
 colnames(wave2)
 
+# pull variables to compare
 colnames1 <- colnames2 <- c("religion_r", "dem", "income_r")
 
+# run t-tests on these three variables
 t_gen_out <- t_generalize(wave1, wave2, colnames1, colnames2)
+library(xtable)
+xtable(t_gen_out)
+
 sum_func_out_w1 <- sum_func(wave1)
 sum_func_out_w2 <- sum_func(wave1)
+
+# can't do much else without the respondent variable names or treatment/control info
+
 
