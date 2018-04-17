a8097b4717e716204f8b64b326713e72988e6651
Loren Collingwood <loren.collingwood@ucr.edu>
2018-04-17 08:01:17 -0700

    Added on geocoding function based on ggmap package. So ggmap now becomes a dependency

--- RTurkcheck.R
+++ RTurkcheck.R
@@ -147,3 +147,44 @@ aov_treat_compare <- function (dat, var_names, treat_var){
   aov_mat <- round(aov_mat, 4)
   return (aov_mat)
 }
+
+############################################################
+# getGeoDetails -- extracting latitude/longitude geocodes  #
+############################################################
+#install.packages("ggmap")
+library(ggmap)
+getGeoDetails <- function(address, source_type="google"){   
+  
+  # address = String/character of address
+  # source_type = "google"/"dsk", character
+  # use the gecode function to query google servers
+  geo_reply = geocode(address, output='all', messaging=TRUE, override_limit=TRUE, source=source_type)
+  #now extract the bits that we need from the returned list
+  answer <- data.frame(lat=NA, long=NA, accuracy=NA, formatted_address=NA, address_type=NA, status=NA)
+  answer$status <- geo_reply$status
+  
+  #if we are over the query limit - want to pause for an hour
+  while(geo_reply$status == "OVER_QUERY_LIMIT"){
+    print("OVER QUERY LIMIT - Pausing for 1 hour at:") 
+    time <- Sys.time()
+    print(as.character(time))
+    Sys.sleep(60*60)
+    geo_reply = geocode(address, output='all', messaging=TRUE, override_limit=TRUE, source=source_type)
+    answer$status <- geo_reply$status
+  }
+  
+  #return Na's if we didn't get a match:
+  if (geo_reply$status != "OK"){
+    return(answer)
+  }   
+  #else, extract what we need from the Google server reply into a dataframe:
+  answer$lat <- geo_reply$results[[1]]$geometry$location$lat
+  answer$long <- geo_reply$results[[1]]$geometry$location$lng   
+  if (length(geo_reply$results[[1]]$types) > 0){
+    answer$accuracy <- geo_reply$results[[1]]$types[[1]]
+  }
+  answer$address_type <- paste(geo_reply$results[[1]]$types, collapse=',')
+  answer$formatted_address <- geo_reply$results[[1]]$formatted_address
+  
+  return(answer)
+}
